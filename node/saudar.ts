1506b9f9cfcf68831ec096a2182a0f6c9b0b0f91
gitwelt <invaodzieo@gmail.com>
2025-01-16 17:13:35 -0300

    commit

--- node/saudar.ts
+++ node/saudar.ts
@@ -1,3 +1,23 @@
-function saudar(nome:string, idade:number){
-   console.log('Olá' + nome)
+interface Pessoa{ 
+   nome: string;
+   idade: number;
+   email?: string;
 }
+
+let pessoas: Pessoa[] = [];
+
+pessoas.push({nome:'Jéssica', idade: 17});
+pessoas.push({nome:'Pedro', idade: 22});
+pessoas.push({nome:'Carlos', idade: 33, email:'carlos992@gmail.com'});
+
+function saudar(){
+   for (let index = 0; index < pessoas.length; index++) {
+      let template = `Olá ${pessoas[index].nome}! Você tem ${pessoas[index].idade} anos`;
+      if ('email' in pessoas[index]) {
+         template = template + `, e seu email é ${pessoas[index].email}!`;
+      }   
+      console.log(template);
+   }
+}
+
+saudar();
